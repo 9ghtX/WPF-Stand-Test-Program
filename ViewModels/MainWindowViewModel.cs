1cb8de823b1664da1db7e148e0d33f1b15a8b780
9ghtX <sevav2002@bk.ru>
2020-09-08 15:35:55 +0700

    Добавил меню сверху. Изменил немного интерфейс программы в целом.
     - Первый элемент меню будет содержать в себе доступные порты
     - Второй элемент меню будет содержать в себе список выриантов Baud Rate
    
     Функционал нижних элементов меню и в целом представление о работе программы будет прикрепленно отдельным текстовым файлом
    
     Во ViewModel добавил небольшую команду по отслеживанию изменений. Пока еще не разобрался, как она работает, но все равно оставлю на этой ветви.
     При надобности можно будет ее удалить

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfApp2.ViewModels.Base;
+
+namespace WpfApp2.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+
+        #region Загловок окна
+
+        /// <summery>Загловок окна</summery>
+
+        private string _Title = "Main Menu";
+
+        public string Title
+        {
+
+            get => _Title;
+
+            set => Set(ref _Title, value);
+
+        }
+        #endregion
+
+    }
+}
